5f2161c051c7e835480aaac264eefc881a979e22
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2026-04-28 09:29:43 +0200

    Moved members list index migration
    ref https://linear.app/ghost/issue/BER-3471/fix-default-members-list-query-performance-with-a-composite-index
    
    The stale PR branch carried the members composite index migration in an old release folder, so this moves the migration onto the current release train while preserving the schema update.

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = 'db036dfc567b48b78bdd36cd6604909f';
+    const currentSchemaHash = '243c9e5afe36caae3a00abd044028575';
     const currentFixturesHash = '911c57c12a164237078b618c5b63042d';
     const currentSettingsHash = 'a102b80d2ab0cd92325ed007c94d7da6';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
